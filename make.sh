41076644003cd7d851aa8ac1435602496823a8d6
adann0 <40341834+adann0@users.noreply.github.com>
2019-05-18 20:45:21 +0200

    Create make.sh

---
+++ make.sh
@@ -0,0 +1,41 @@
+
+DOMAIN=example.com
+
+# MySQL
+mkdir -p mnt/data/mysql
+
+# NextCloud
+mkdir -p mnt/config/cloud
+mkdir -p mnt/data/cloud
+mkdir -p mnt/www/$DOMAIN/cloud/apps
+mkdir -p mnt/www/$DOMAIN/cloud/themes
+
+# Nginx
+mkdir -p mnt/config/nginx
+mkdir -p mnt/config/ssl/certs
+
+# Portainer
+mkdir -p mnt/data/portainer
+
+# OpenLDAP
+mkdir -p mnt/data/openldap/ldap
+mkdir -p mnt/data/openldap/slapd
+
+# MediaServer
+mkdir -p mnt/data/media/movie/Films
+mkdir -p mnt/data/media/movie/Series
+mkdir -p mnt/data/media/movie/Deposit
+mkdir -p mnt/data/media/music
+
+# Config
+mkdir -p mnt/config/deluge
+mkdir -p mnt/config/hydra2
+mkdir -p mnt/config/jackett
+mkdir -p mnt/config/daapd
+mkdir -p mnt/config/plex
+mkdir -p mnt/config/samba
+mkdir -p mnt/config/sonarr
+mkdir -p mnt/config/radarr
+mkdir -p mnt/config/bazarr
+
+
